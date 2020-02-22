6f4cfb7aa12a673310efc11295a037f614816b1e
AlastairM-E <alastairunityemail@gmail.com>
2020-02-22 19:54:58 +0000

    aded a small test area and appriaote commands, will add arguments soon

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
-const { exec, } = require('child_process');
+const { exec } = require('child_process');
+const { argv } = require('process');
 
 const cmd = `
     # basic npm structure
     $PWD = pwd;
+
+    yarn add --dev style-loader css-loader node-sass sass-loader html-loader file-loader
+    yarn add --dev  webpack webpack-cli webpack-merge webpack-dev-server
+    yarn add --dev mini-css-extract-plugin clean-webpack-plugin  html-webpack-plugin
+    yarn add --dev @babel/core @babel/preset-env @babel/preset-react babel-loader
+    yarn add react react-dom
+    yarn add --dev jest
+
     # webpack and babel basic build
     cp /Users/alastair/Documents/shell__commands/webpack.common.js "$PWD"
     cp /Users/alastair/Documents/shell__commands/webpack.dev.js "$PWD"
@@ -16,9 +25,10 @@ const cmd = `
     cp /Users/alastair/Documents/shell__commands/src/App.js "$PWD/src"
     cp /Users/alastair/Documents/shell__commands/src/template.html "$PWD/src"
     cp /Users/alastair/Documents/shell__commands/src/app.test.js "$PWD/src"
-`
+`;
 
 exec(cmd, (error) => { 
+    console.log(argv);
     if (error) {
         throw error
     };
