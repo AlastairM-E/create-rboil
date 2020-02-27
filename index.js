1f02dc7107bedfdb600b1e60d7d2f02bdec8317e
AlastairM-E <alastairunityemail@gmail.com>
2020-02-27 22:25:56 +0000

    license has been added

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ const cmd = `
     yarn add --dev style-loader css-loader node-sass sass-loader html-loader file-loader
     yarn add --dev  webpack webpack-cli webpack-merge webpack-dev-server
     yarn add --dev mini-css-extract-plugin clean-webpack-plugin  html-webpack-plugin
-    yarn add --dev @babel/core @babel/preset-env @babel/preset-react babel-loader
-    yarn add react react-dom
+    yarn add --dev @babel/core @babel/preset-env @babel/preset-react @babel/plugin-transform-runtime @babel/plugin-syntax-dynamic-import babel-loader
     yarn add --dev jest
+    yarn add --dev create-rboil-utils
+    yarn add react react-dom
 `;
+
+const pathOfCmd = process.cwd().split('/')
+const nameOfCmdDir = pathOfCmd[pathOfCmd.length - 1].toLowerCase();
 
 const templateFiles = readSubFilesFrom(path.join(__dirname, 'template'));
 
@@ -53,44 +57,48 @@ npm-debug.log*
 yarn-debug.log*
 yarn-error.log*`
 );
+
 fs.writeFileSync(`${process.cwd()}/package.json`, `{
-    "name": "xyz",
-    "version": "1.0.0",
-    "main": "index.js",
-    "license": "MIT",
-    "devDependencies": {
-      "@babel/core": "^7.8.4",
-      "@babel/preset-env": "^7.8.4",
-      "@babel/preset-react": "^7.8.3",
-      "babel-loader": "^8.0.6",
-      "clean-webpack-plugin": "^3.0.0",
-      "css-loader": "^3.4.2",
-      "file-loader": "^5.1.0",
-      "html-loader": "^0.5.5",
-      "html-webpack-plugin": "^3.2.0",
-      "jest": "^25.1.0",
-      "mini-css-extract-plugin": "^0.9.0",
-      "node-sass": "^4.13.1",
-      "sass-loader": "^8.0.2",
-      "style-loader": "^1.1.3",
-      "webpack": "^4.41.6",
-      "webpack-cli": "^3.3.11",
-      "webpack-dev-server": "^3.10.3",
-      "webpack-merge": "^4.2.2"
-    },
-    "dependencies": {
-      "react": "^16.12.0",
-      "react-dom": "^16.12.0"
-    },
-    "scripts": {
-        "start": "webpack-dev-server --config ./dev-scripts/webpack.dev.js --open",
-        "build": "webpack --config ./dev-scripts/webpack.prod.js",
-        "test": "./dev-scripts/bundleTests.js && jest ./test && rm -r test/*"
-      },
-      "jest": {
-        "transform": {
-          "^.+\\\\.[t|j]sx?$": "babel-jest"
-        }
-      }
+  "name": "${nameOfCmdDir}",
+  "version": "1.0.0",
+  "main": "index.js",
+  "license": "MIT",
+  "devDependencies": {
+    "@babel/core": "^7.8.4",
+    "@babel/plugin-syntax-dynamic-import": "^7.8.3",
+    "@babel/plugin-transform-runtime": "^7.8.3",
+    "@babel/preset-env": "^7.8.4",
+    "@babel/preset-react": "^7.8.3",
+    "babel-loader": "^8.0.6",
+    "clean-webpack-plugin": "^3.0.0",
+    "create-rboil-utils": "^1.1.0",
+    "css-loader": "^3.4.2",
+    "file-loader": "^5.1.0",
+    "html-loader": "^0.5.5",
+    "html-webpack-plugin": "^3.2.0",
+    "jest": "^25.1.0",
+    "mini-css-extract-plugin": "^0.9.0",
+    "node-sass": "^4.13.1",
+    "sass-loader": "^8.0.2",
+    "style-loader": "^1.1.3",
+    "webpack": "^4.41.6",
+    "webpack-cli": "^3.3.11",
+    "webpack-dev-server": "^3.10.3",
+    "webpack-merge": "^4.2.2"
+  },
+  "dependencies": {
+    "react": "^16.12.0",
+    "react-dom": "^16.12.0"
+  },
+  "scripts": {
+    "start": "webpack-dev-server --config ./dev-scripts/webpack.dev.js --open",
+    "build": "webpack --config ./dev-scripts/webpack.prod.js",
+    "test": "node ./dev-scripts/bundleTests.js && jest ./test && rm -rf test/*"
+  },
+  "jest": {
+    "transform": {
+      "^.+\\.[t|j]sx?$": "babel-jest"
+    }
   }
+}
 `);
