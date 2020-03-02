e19d3751137d2260fd7518343fb16a08a7dad1ab
AlastairM-E <alastairunityemail@gmail.com>
2020-03-02 14:18:57 +0000

    made the cli nicer, got rid of the .gitignore creation. Now need to test with the package will work without yarn.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ exec('yarn --version', (err, stdout) => {
       throw err;
   };
 
+  console.log('This may take a couple of minutes, sorry for the wait.')
+
   const cmdInit = stdout ? 'yarn init -y' : 'npm init -y'; 
   const addDevDependencies = stdout ? 'yarn add --dev' : 'npm install --save-dev';
   const addProductionDependencies = stdout ? 'yarn add' : 'npm install';
@@ -46,35 +48,6 @@ exec('yarn --version', (err, stdout) => {
   const templateFiles = readSubFilesFrom(path.join(__dirname, 'template'));
 
   outputDirContentOf(templateFiles, path.join(__dirname, 'template'), `${process.cwd()}`);
-
-
-  fs.writeFileSync(`${process.cwd()}/.gitignore`, 
-  `# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
-
-  # dependencies
-  /node_modules
-  /dist
-  /.pnp
-  /yarn.lock
-  .pnp.js
-
-  # testing
-  /coverage
-
-  # production
-  /build
-
-  # misc
-  .DS_Store
-  .env.local
-  .env.development.local
-  .env.test.local
-  .env.production.local
-
-  npm-debug.log*
-  yarn-debug.log*
-  yarn-error.log*`
-  );
 
   fs.writeFileSync(`${process.cwd()}/package.json`, `{
     "name": "${nameOfCmdDir}",
